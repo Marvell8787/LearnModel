0d1831479db81675965c35151eaa447a624816b3
Marvell8787 <wendy850122@gmail.com>
2019-03-22 20:57:41 +0800

    20190322

---
+++ LearnModel/Assets/Script/Challenge/Level-1/Image_C2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Image_C2 : MonoBehaviour {
+
+    public AudioSource Audiosource_1; //goodbye
+    public AudioSource Audiosource_2; //hi
+    public AudioSource Audiosource_3; //how are you
+    public AudioSource Audiosource_4; //let's go
+    public AudioSource Audiosource_5; //thank you
+    public AudioSource Audiosource_6; //You're welcome
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Click()
+    {
+        Audiosource_1.Play();
+        Debug.Log("get");
+
+
+    }
+
+}
